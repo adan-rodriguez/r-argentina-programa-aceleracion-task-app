891c1ba90434e4fd016b99065ca69c1a7ed50bf8
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2022-11-30 12:31:12 -0300

    Add subtitle

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Task App</h1>
+      <h2>Organiza tus tareas en un solo lugar</h2>
       <img src={Logo} alt="Logo" className="logo" />
       <TaskApp />
       <Footer />
